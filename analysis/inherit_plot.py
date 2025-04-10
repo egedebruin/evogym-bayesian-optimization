142712895de5e5afda6dfb290e1801b31f3ac56d
Ege <eged@uio.no>
2025-04-10 13:34:01 +0200

    Add inherit plot and change some config parameters

---
+++ analysis/inherit_plot.py
@@ -0,0 +1,64 @@
+import math
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import plot
+
+plt.figure(figsize=(8, 5))
+pop_size = 100
+
+to_label = {
+    (-1, 1): 'Evolution only',
+    (-1, 30): 'Learn - No inheritance',
+    (0, 30): 'Learn - Inherit samples',
+    (5, 30): 'Learn - Reevaluate'
+}
+to_color = {
+    (-1, 1): 'red',
+    (-1, 30): 'blue',
+    (0, 30): 'green',
+    (5, 30): 'orange'
+}
+
+max_y = []
+for inherit in [-1, 0, 5]:
+    for generations, learn in [(1000, 1), (33, 30)]:
+        print("Plotting for inherit {} and learn {}".format(inherit, learn))
+        if learn == 1 and inherit != -1:
+            continue
+        to_plot = []
+        for repetition in range(1, 21):
+            data_array = plot.get_data(f'results/learn-{str(learn)}_inherit-{str(inherit)}_repetition-{str(repetition)}')
+            if len(data_array) < generations:
+                print(learn, inherit, repetition)
+                continue
+
+            max_values = np.max(data_array, axis=1)
+            mean_values = np.mean(data_array, axis=1)
+            max_so_far = np.array([])
+            for value in max_values:
+                max_so_far = np.append(max_so_far, max(value, max(list(max_so_far) + [-math.inf])))
+
+            to_plot.append(max_so_far)
+
+        function_evaluations = np.array(range(1, generations + 1))
+        function_evaluations = function_evaluations * learn * pop_size
+        mean_to_plot = np.mean(to_plot, axis=0)
+        q1 = np.percentile(to_plot, 1, axis=0)
+        q25 = np.percentile(to_plot, 25, axis=0)
+        q75 = np.percentile(to_plot, 75, axis=0)
+        q99 = np.percentile(to_plot, 99, axis=0)
+        plt.plot(function_evaluations, mean_to_plot, label=to_label[(inherit, learn)], color=to_color[(inherit, learn)])
+        plt.fill_between(function_evaluations, q25, q75, alpha=0.3, color=to_color[(inherit, learn)])
+        # plt.fill_between(function_evaluations, q1, q99, alpha=0.1, color=to_color[(inherit, learn)])
+        # plt.plot(function_evaluations, q1, linestyle='dashed', color=to_color[(inherit, learn)])
+        # plt.plot(function_evaluations, q99, linestyle='dashed', color=to_color[(inherit, learn)])
+        max_y.append(max(q99) * 1.1)
+
+plt.ylim(0, max(max_y))
+plt.xlabel("Function evaluations")
+plt.ylabel("Objective value")
+plt.legend()
+plt.title("Max performance averaged over repetitions")
+plt.show()
--- analysis/plot.py
+++ analysis/plot.py
@@ -7,44 +7,51 @@ parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, parent_dir)
 import config
 
-individuals_file = open(config.FOLDER + "individuals.txt", "r")
-all_individuals = {individual.split(";")[0]: float(individual.split(";")[5]) for individual in individuals_file.read().splitlines()}
+def get_data(folder):
+    individuals_file = open(folder + "/individuals.txt", "r")
+    all_individuals = {individual.split(";")[0]: float(individual.split(";")[5]) for individual in individuals_file.read().splitlines()}
 
-populations_file = open(config.FOLDER + "populations.txt", "r")
-generations = populations_file.read().splitlines()
+    populations_file = open(folder + "/populations.txt", "r")
+    generations = populations_file.read().splitlines()
 
-objective_values_per_generation = []
-for generation in generations:
-    generation_objective_values = []
-    for individual in generation.split(";")[:-1]:
-        generation_objective_values.append(all_individuals[individual])
-    objective_values_per_generation.append(generation_objective_values)
+    objective_values_per_generation = []
+    for generation in generations:
+        generation_objective_values = []
+        for individual in generation.split(";")[:-1]:
+            generation_objective_values.append(all_individuals[individual])
+        objective_values_per_generation.append(generation_objective_values)
 
-# Convert to numpy array for easy computation
-data_array = np.array(objective_values_per_generation)
+    # Convert to numpy array for easy computation
+    return np.array(objective_values_per_generation)
 
-# Calculate mean and standard deviation along the rows
-max_values = np.max(data_array, axis=1)
-mean_values = np.mean(data_array, axis=1)
-std_values = np.std(data_array, axis=1)
+def main():
+    data_array = get_data(config.FOLDER)
 
-max_so_far = []
-previous = 0
-for value in max_values:
-    best = max(value, previous)
-    max_so_far.append(best)
-    previous = best
-max_so_far = np.array(max_so_far)
+    # Calculate mean and standard deviation along the rows
+    max_values = np.max(data_array, axis=1)
+    mean_values = np.mean(data_array, axis=1)
+    std_values = np.std(data_array, axis=1)
 
-# Plot
-plt.figure(figsize=(8, 5))
-plt.plot(max_so_far, label="Max accumulated", marker='o', color='red')
-plt.plot(max_values, label="Max", marker='o', color='blue')
-plt.plot(mean_values, label="Mean", marker='o', color='green')
-plt.fill_between(range(len(mean_values)), mean_values - std_values, mean_values + std_values, alpha=0.2, color='green')
-plt.ylim(0, max(max_values) + 5)
-plt.xlabel("Generation")
-plt.ylabel("Objective value")
-plt.legend()
-plt.title("Max, mean and Standard Deviation")
-plt.show()
+    max_so_far = []
+    previous = 0
+    for value in max_values:
+        best = max(value, previous)
+        max_so_far.append(best)
+        previous = best
+    max_so_far = np.array(max_so_far)
+
+    # Plot
+    plt.figure(figsize=(8, 5))
+    plt.plot(max_so_far, label="Max so far", marker='o', color='red')
+    plt.plot(max_values, label="Max", marker='o', color='blue')
+    plt.plot(mean_values, label="Mean", marker='o', color='green')
+    plt.fill_between(range(len(mean_values)), mean_values - std_values, mean_values + std_values, alpha=0.2, color='green')
+    plt.ylim(0, max(max_values) + 5)
+    plt.xlabel("Generation")
+    plt.ylabel("Objective value")
+    plt.legend()
+    plt.title("Max, mean and standard deviation")
+    plt.show()
+
+if __name__ == '__main__':
+    main()
