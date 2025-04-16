a91ce63d5f564c27d600379700a05b729aa01fd9
Ege <eged@uio.no>
2025-04-16 16:57:38 +0200

    Move decision of type of controller to config

--- robot/active.py
+++ robot/active.py
@@ -1,5 +1,10 @@
-# from robot.brain_nn import BrainNN as Brain
-# from robot.controller_nn import ControllerNN as Controller
+import config
 
-from robot.brain_sine import BrainSine as Brain
-from robot.controller_sine import ControllerSine as Controller
+if config.BRAIN_TYPE == 'nn':
+    from robot.brain_nn import BrainNN as Brain
+    from robot.controller_nn import ControllerNN as Controller
+elif config.BRAIN_TYPE == 'sine':
+    from robot.brain_sine import BrainSine as Brain
+    from robot.controller_sine import ControllerSine as Controller
+else:
+    raise ValueError('BRAIN_TYPE does not exist')
