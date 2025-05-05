a904b6eaadbe59f6da42d9d01f0913d9e45c134c
Ege <eged@uio.no>
2025-05-05 13:31:27 +0200

    more random env tester

--- environment_tester.py
+++ environment_tester.py
@@ -64,10 +64,7 @@ def learn(rng, body, learn_iterations):
         f=None,
         pbounds=brain.get_p_bounds(actuator_indices),
         allow_duplicate_points=True,
-        random_state=int(rng.integers(low=0, high=2 ** 32)),
-        acquisition_function=acquisition.UpperConfidenceBound(kappa=config.LEARN_KAPPA,
-                                                              random_state=int(
-                                                                  rng.integers(low=0, high=2 ** 32)))
+        acquisition_function=acquisition.UpperConfidenceBound(kappa=config.LEARN_KAPPA)
     )
     optimizer.set_gp_params(
         kernel=Matern(nu=config.LEARN_NU, length_scale=config.LEARN_LENGTH_SCALE, length_scale_bounds="fixed"))
@@ -75,10 +72,7 @@ def learn(rng, body, learn_iterations):
 
     for bayesian_optimization_iteration in range(learn_iterations):
         print(f"Learn generation {bayesian_optimization_iteration + 1}")
-        if bayesian_optimization_iteration == 0:
-            next_point = brain.to_next_point(actuator_indices)
-        else:
-            next_point = optimizer.suggest()
+        next_point = optimizer.suggest()
 
         args = brain.next_point_to_controller_values(next_point, actuator_indices)
         controller = Controller(args)
