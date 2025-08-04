d2026206e33fd8a4fb92b55671b1bac05aeb2f03
Ege <eged@uio.no>
2025-08-04 14:51:00 +0200

    Add restart BO thingy to config

--- custom_bayesian_optimization.py
+++ custom_bayesian_optimization.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 from bayes_opt import BayesianOptimization
 
+from configs import config
+
 
 class CustomBayesianOptimization(BayesianOptimization):
 
@@ -10,6 +12,6 @@ class CustomBayesianOptimization(BayesianOptimization):
             return self._space.array_to_params(self._space.random_sample())
 
         # Finding argmax of the acquisition function.
-        suggestion = self._acquisition_function.suggest(gp=self._gp, target_space=self._space, fit_gp=True, n_l_bfgs_b=1)
+        suggestion = self._acquisition_function.suggest(gp=self._gp, target_space=self._space, fit_gp=True, n_l_bfgs_b=config.BO_RESTARTS)
 
         return self._space.array_to_params(suggestion)
