52411ac0489f82d4351d51de593e22219ff1766c
Ege <eged@uio.no>
2026-06-08 11:09:58 +0200

    run optimized robots bidirectional

---
+++ optimized_robots_bidirectional.py
@@ -0,0 +1,54 @@
+import os
+
+# Force single-threaded execution
+# DO THIS BEFORE IMPORTS
+os.environ["OMP_NUM_THREADS"] = "1"       # OpenMP
+os.environ["MKL_NUM_THREADS"] = "1"       # Intel MKL
+os.environ["OPENBLAS_NUM_THREADS"] = "1"  # OpenBLAS
+os.environ["NUMEXPR_NUM_THREADS"] = "1"   # NumExpr
+
+import pickle
+from util import restart_population
+from util.logger_setup import logger_setup
+from configs import config
+from main import get_offspring, run_generation
+from selection import Selection
+from robot.brain_nn import BrainNN
+
+config.FOLDER = ''
+logger_setup()
+
+config.OFFSPRING_SIZE = 100
+config.MIN_MUTATION = 0
+config.MAX_MUTATION = 2
+config.PARALLEL_PROCESSES = 100
+config.LEARN_ITERATIONS = 100
+config.GLOBAL_CONTROLLER = False
+config.MODULAR_NEIGHBOUR_VISION = 2
+
+BrainNN.set_modular_vision()
+
+results = {
+    'inherit': [],
+    'repetition': [],
+    'individual': [],
+    'values': [],
+}
+for inherit in [(-1, 'none', 0), (1, 'parent', 1)]:
+    for repetition in range(1, 21):
+        config.FOLDER = f'results/learn-100_inherit-{inherit[0]}_type-{inherit[1]}_pool-{inherit[2]}_environment-bidirectional_method-ddpg_vision-2_repetition-{repetition}/'
+
+        population, generation_index = restart_population.get_population()
+        rng = restart_population.get_rng()
+        parent_selection = Selection(config.OFFSPRING_SIZE, config.PARENT_SELECTION, {'pool_size': config.PARENT_POOL})
+        offspring = get_offspring(population, generation_index, parent_selection, rng)
+        evaluated_offspring, _ = run_generation(offspring, None, rng)
+
+        for i, individual in enumerate(evaluated_offspring):
+            results['inherit'].append(inherit[0])
+            results['repetition'].append(repetition)
+            results['individual'].append(i)
+            results['values'].append([value for _, value in individual.experience])
+
+with open("results/bidirectional-delta-results.pkl", "wb") as f:
+    pickle.dump(results, f)
